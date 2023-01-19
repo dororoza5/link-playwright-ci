c7071c11af027a7f41d48423986cd9ab3ad60990
Ruttasate <m18260.29@gmail.com>
2023-01-19 19:08:48 +0900

    test CI#17

--- test-playwright/tests/example.spec.ts
+++ test-playwright/tests/example.spec.ts
@@ -1,25 +1,26 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole("link", { name: "Get started" }).click();
 
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
 });
 
-test("local", async ({page}) => {
-  await page.goto("http://localhost:5173/")
+test("local", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
 
   // await page.waitForSelector("//h1[normalize-space()='Vite + React']");
-  await expect(page).toHaveTitle("Vite + React")
-})
+  await expect(page).toHaveTitle("Vite + React");
+  await page.getByRole("button", { name: /count is/ }).click();
+});
